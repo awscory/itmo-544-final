a5b4539114c6980da6569576ad2685f739b1a10f
Sukanya Nagarajan <rajansuki@gmail.com>
2015-10-17 20:07:29 -0500

    Security groups misspelled in create elb. corrected it

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 
 #Step 1 : create instances and run
 
-aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 --associate-public-ip-address --user-data file://EnvSetUp/install-env.sh --debug
+aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 --associate-public-ip-address --user-data file://EnvSetUp/install-env.sh --debug > ./itmo-544-final-create-log.txt
 for i in 0..150
 do 
 	echo -ne '.'
@@ -10,10 +10,10 @@ do
 done
 
 #Step 2 : Decribe instances 
-aws ec2 describe-instanes  > ./itmo-544-final-launch-log.txt
+aws ec2 describe-instances  > ./itmo-544-final-launch-log.txt
 
 #Step 3: Create load Balancer
-aws elb create-load-balancer --load-balancer-name $7 --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --subnets $6 --security-group-ids $5
+aws elb create-load-balancer --load-balancer-name $7 --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --subnets $6 --security-groups $5
 
 
 

52d953a2b4a31f283795add029726518d7f2b446
Sukanya Nagarajan <rajansuki@gmail.com>
2015-10-18 10:43:48 -0500

    Added for loop to sleep for sometime

--- launch.sh
+++ launch.sh
@@ -5,6 +5,12 @@ declare -a InstArr
 
 aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 --associate-public-ip-address --user-data file://EnvSetUp/install-env.sh  
 
+for var1 in {0..150}
+do
+ echo -ne "."
+	sleep 1
+done
+
 
 #Step 2 : Decribe instances 
 
@@ -15,6 +21,8 @@ mapfile -t InstArr < <(aws ec2 describe-instances --filter Name=instance-state-c
 
 #Step 3: Create load Balancer
 aws elb create-load-balancer --load-balancer-name $7 --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --subnets $6 --security-groups $5
+
+
 
 
 

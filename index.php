9db019c3ba53c5d66271c4480d6906d06f250fb4
Sukanya Nagarajan <rajansuki@gmail.com>
2015-10-24 21:33:00 -0500

    changed index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ session_start();
 
 <form enctype="multipart/form-data" action="submit.php" method="post">
 User Name: <input type="text" name="username" value="sukanya"><br>
-E-mail: <input type="text" name="email" value="snagar15@hawk.iit.edu"><br>
+E-mail: <input type="text" name="email" value="****@***.iit.edu"><br>
 Phone : <input type="text" name="phone" value="312-000-0000"><br>
 <input type="hidden" name="MAX_FILE_SIZE" value="3000000"><br>
 Your File : <input type="file" name="choose a file">

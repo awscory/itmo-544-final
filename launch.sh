306bce367f84426a58c0f608fa9ed6e4989b80ce
Sukanya Nagarajan <rajansuki@gmail.com>
2015-10-28 21:13:20 -0500

    removed read replica creation for now

--- launch.sh
+++ launch.sh
@@ -77,12 +77,12 @@ aws rds create-db-instance --db-name itmo544SukanyaMySql --db-instance-identifie
 
 # wait for the DB instance to be available
 echo "waiting for the Db instance to be available"
-#aws rds wait db-instance-available --db-instance-identifier itmo-544-SN-db 
+aws rds wait db-instance-available --db-instance-identifier itmo-544-SN-db 
  
 
 #Create Read replica of the Db instance in the same region
 echo "creating read replica"
-aws rds create-db-instance-read-replica --db-instance-identifier itmo-544-SN-dbreplica --source-db-instance-identifier itmo-544-SN-db --db-instance-class db.t1.micro --availability-zone us-west-2a
+#aws rds create-db-instance-read-replica --db-instance-identifier itmo-544-SN-dbreplica --source-db-instance-identifier itmo-544-SN-db --db-instance-cass db.t1.micro --availability-zone us-west-2a
 
 # wait for read replica to be available
 echo "waiting for read replica to be available"

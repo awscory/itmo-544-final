ccbdd303837af0293206d5ad491953b10a2ba9a8
Sukanya Nagarajan <rajansuki@gmail.com>
2015-11-01 09:56:17 -0600

    added rds connection and table creation statement

--- launch.sh
+++ launch.sh
@@ -108,7 +108,7 @@ aws rds create-db-subnet-group --db-subnet-group-name dbsgnameSN --db-subnet-gro
 
 #Step Create DB instance
 echo "Creating DB instance"
-aws rds create-db-instance --db-name itmo544SukanyaMySql --db-instance-identifier itmo-544-SN-db --allocated-storage 20 --db-instance-class db.t1.micro --engine MYSQL --master-username SukanyaN --master-user-password SukanyaNDB --vpc-security-group-ids $5 --availability-zone us-west-2b  --db-subnet-group-name dbsgnameSN
+$result= aws rds create-db-instance --db-name itmo544SukanyaMySql --db-instance-identifier itmo-544-SN-db --allocated-storage 20 --db-instance-class db.t1.micro --engine MYSQL --master-username SukanyaN --master-user-password SukanyaNDB --vpc-security-group-ids $5 --availability-zone us-west-2b  --db-subnet-group-name dbsgnameSN
 
 # wait for the DB instance to be available
 echo "waiting for the Db instance to be available"
@@ -123,7 +123,38 @@ echo "creating read replica"
 echo "waiting for read replica to be available"
 #aws rds wait db-instance-available --db-instance-identifier itmo-544-SN-dbreplica
 
-php ./itmo-544-final/setup.php
+#get DB endpoint
+$endpoint = $result['DBInstances'][0]['Endpoint']['Address']
+
+echo "============\n". $endpoint . "================";
+
+echo "begin database";
+$link = mysqli_connect($endpoint,"SukanyaN","SukanyaNDB","items") or die("Error ". mysqli_error($link));
+
+echo "LInk is $link" 
+
+$sql = "CREATE TABLE items 
+(
+ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+UName VARCHAR(20),
+Email Varchar(20),
+Phone Varchar(20),
+RawS3Url  Varchar(256),
+FinalS3Url  Varchar(256),
+JpgFileName    Varchar(256),
+Status    TinyInt(3),
+Issubscribed TinyInt(3),
+CreationTime  Timestamp
+)";
+
+$con->query($sql);
+
+echo " table done";
+
+
+#sudo apt-get install php5-cli
+
+#php ./itmo-544-final/setup.php
 
 echo "ALL DONE"
 

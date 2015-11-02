8461ecabc88f2035b3c200c7cc6ffb646344e824
Sukanya Nagarajan <rajansuki@gmail.com>
2015-11-01 20:23:48 -0600

    modified file to include dbcreate.sh

--- launch.sh
+++ launch.sh
@@ -124,38 +124,7 @@ echo "creating read replica"
 echo "waiting for read replica to be available"
 #aws rds wait db-instance-available --db-instance-identifier itmo-544-SN-dbreplica
 
-#get DB endpoint
-$endpoint = $result['DBInstances'][0]['Endpoint']['Address']
-
-echo "============\n". $endpoint . "================";
-
-echo "begin database";
-$link = mysqli_connect($endpoint,"SukanyaN","SukanyaNDB","items") or die("Error ". mysqli_error($link));
-
-echo "LInk is $link" 
-
-$sql = "CREATE TABLE items 
-(
-ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-UName VARCHAR(20),
-Email Varchar(20),
-Phone Varchar(20),
-RawS3Url  Varchar(256),
-FinalS3Url  Varchar(256),
-JpgFileName    Varchar(256),
-Status    TinyInt(3),
-Issubscribed TinyInt(3),
-CreationTime  Timestamp
-)";
-
-$con->query($sql);
-
-echo " table done";
-
-
-#sudo apt-get install php5-cli
-
-#php ./itmo-544-final/setup.php
+./itmo-544-final/dbcreate.sh
 
 echo "ALL DONE"
 

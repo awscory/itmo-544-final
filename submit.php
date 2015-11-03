9333a2d61c07d26e09449fc82d54afe4d81e206f
Sukanya Nagarajan <rajansuki@gmail.com>
2015-11-02 18:22:12 -0600

    resolving errors

--- submit.php
+++ submit.php
@@ -1,18 +1,8 @@
 <?php
-require 'vendor/autoload.php';
+
 print_r($_POST);
 echo "test";
 ?>
-<!DOCTYPE html>
-<html>
-<body>
-
-<?php
-// Set session variables
-echo $_POST["email"];
-?>
-
-</body>
-</html> 
+ 
 
 

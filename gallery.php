d94b171d1dd356e72dfb3565f333c1ef32f939c1
Sukanya Nagarajan <rajansuki@gmail.com>
2015-11-04 11:56:46 -0600

    JS file path error resolved

--- gallery.php
+++ gallery.php
@@ -73,8 +73,8 @@ if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
         //this will append the path of images to an array
-        $imgLocations[$row["JpgFileName"]] = $row["RawS3URL"];
-        echo "id: " . $row["ID"]."- RawS3URL" . $row["RawS3URL"]. "<br>";
+        $imgLocations[] = $row["RawS3URL"];
+//        echo "id: " . $row["ID"]."- RawS3URL" . $row["RawS3URL"]. "<br>";
     }
 } 
 else {

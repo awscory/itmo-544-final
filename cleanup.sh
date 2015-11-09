1548c307b2e9d66643c03b8cc4cb34aff27012dc
Sukanya Nagarajan <rajansuki@gmail.com>
2015-11-09 09:12:06 -0600

    autoscaling group and cloud watch added final

--- cleanup.sh
+++ cleanup.sh
@@ -12,6 +12,8 @@ echo "the output is ${cleanupARR[@]}"
 
 aws ec2 terminate-instances --instance-ids ${cleanupARR[@]} 
 
+aws ec2 wait instance-terminated --instance-ids ${cleanupARR[@]}
+
 
 echo "Cleaning up existing Load Balancers"
 mapfile -t cleanupLBARR < <(aws elb describe-load-balancers --output json | grep LoadBalancerName | sed "s/[\"\:\, ]//g" | sed "s/LoadBalancerName//g")
@@ -35,19 +37,6 @@ for (( i=0; i<${LENGTH}; i++));
 done
 
 
-
-if [ ${#SCALENAME[@]} -gt 0 ]
-  then
-echo "SCALING GROUPS to delete..."
-#aws autoscaling detach-launch-
-
-aws autoscaling update-auto-scaling-group --auto-scaling-group-name $SCALENAME --min-size 0 --max-size 0
-
-aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $SCALENAME
-
-aws autoscaling delete-launch-configuration --launch-configuration-name $LAUNCHCONF
-fi
-
 mapfile -t dbInstanceARR < <(aws rds describe-db-instances --output json | grep "\"DBInstanceIdentifier" | sed "s/[\"\:\, ]//g" | sed "s/DBInstanceIdentifier//g" )
 
 if [ ${#dbInstanceARR[@]} -gt 0 ]
@@ -66,4 +55,22 @@ fi
 
 aws rds delete-db-subnet-group --db-subnet-group-name dbsgnameSN
 
+LAUNCHCONF=(`aws autoscaling describe-launch-configurations --output json | grep LaunchConfigurationName | sed "s/[\"\:\, ]//g" | sed "s/LaunchConfigurationName//g"`)
+
+SCALENAME=(`aws autoscaling describe-auto-scaling-groups --output json | grep AutoScalingGroupName | sed "s/[\"\:\, ]//g" | sed "s/AutoScalingGroupName//g"`)
+
+echo "The asgs are: " ${SCALENAME[@]}
+echo "the number of asgs is: " ${#SCALENAME[@]}
+
+if [ ${#SCALENAME[@]} -gt 0 ]
+  then
+  echo "delete scale group"
+  aws autoscaling update-auto-scaling-group --auto-scaling-group-name $SCALENAME --max-size 0 --min-size 0
+  aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $SCALENAME
+  aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $SCALENAME --force-delete
+  aws autoscaling delete-launch-configuration --launch-configuration-name $LAUNCHCONF
+  aws cloudwatch delete-alarms --alarm-name  IncreaseInst DecreaseInst
+fi
 echo "All done"
+
+
--- launch.sh
+++ launch.sh
@@ -94,10 +94,18 @@ aws elb create-lb-cookie-stickiness-policy --load-balancer-name $7 --policy-name
 
 #Step Create Auto scaling group
 echo "creating launch configuration"
-aws autoscaling create-launch-configuration --launch-configuration-name itmo544-launch-config --image-id $1 --key-name $4  --security-groups $5 --instance-type $3 --user-data file://EnvSetUp/install-env.sh --iam-instance-profile $8
+aws autoscaling create-launch-configuration --launch-configuration-name itmo544-launch-config --image-id $1 --key-name $4  --security-groups $5 --instance-type $3 --associate-public-ip-address --user-data file://EnvSetUp/install-env.sh --iam-instance-profile $8
 
 echo "creating auto scaling group"
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name itmo-544-Sukanya-auto-scaling-group-2 --launch-configuration-name itmo544-launch-config --load-balancer-names $7  --health-check-type ELB --min-size 1 --max-size 3 --desired-capacity 2 --default-cooldown 600 --health-check-grace-period 120 --vpc-zone-identifier $6 
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name itmo-544-Sukanya-auto-scaling-group-2 --launch-configuration-name itmo544-launch-config --load-balancer-names $7  --health-check-type ELB --min-size 3 --max-size 6 --desired-capacity 3 --default-cooldown 600 --health-check-grace-period 120 --vpc-zone-identifier $6 
+
+SCALEUP=(`aws autoscaling put-scaling-policy --policy-name SCALEUP --auto-scaling-group-name itmo-544-Sukanya-auto-scaling-group-2 --scaling-adjustment 1 --adjustment-type ChangeInCapacity`)
+aws cloudwatch put-metric-alarm --alarm-name IncreaseInst --alarm-description "when CPU usage is greaterthanorequal to 30 percent" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 120 --threshold 30 --comparison-operator GreaterThanOrEqualToThreshold --evaluation-periods 1 --dimensions "Name=AutoScalingGroupName,Value=itmo-544-Sukanya-auto-scaling-group-2" --unit Percent --alarm-actions $SCALEUP
+
+#Create auto scaling policy to monitor when CPU usage is below or equal to 10 percent 
+SCALEDOWN=(`aws autoscaling put-scaling-policy --policy-name SCALEDOWN --auto-scaling-group-name itmo-544-Sukanya-auto-scaling-group-2 --scaling-adjustment -1 --adjustment-type ChangeInCapacity`)
+aws cloudwatch put-metric-alarm --alarm-name DecreaseInst --alarm-description "when CPU usage lessthanorequal to 10 percent" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 120 --threshold 10 --comparison-operator LessThanOrEqualToThreshold  --evaluation-periods 1 --dimensions "Name=AutoScalingGroupName,Value=itmo-544-Sukanya-auto-scaling-group-2" --unit Percent --alarm-actions $SCALEDOWN
+
 
 #Step Creating RDS db-subnet-group
 echo "creating DB-subnet-group"

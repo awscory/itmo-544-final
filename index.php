1514572151770f9b0e7080f9297e5931378550c0
Sukanya Nagarajan <rajansuki@gmail.com>
2015-11-20 22:56:54 -0600

    set up session variables

--- index.php
+++ index.php
@@ -4,20 +4,25 @@ session_start();
 
 <html>
 <body>
-
 <form enctype="multipart/form-data" action="submit.php" method="post">
 User Name: <input type="text" name="username" value="sukanya"><br>
 E-mail: <input type="text" name="email" value="****@***.iit.edu"><br>
 Phone : <input type="text" name="phone" value="312-000-0000"><br>
 <input type="hidden" name="MAX_FILE_SIZE" value="3000000"><br>
 Your File : <input type="file" name="userfile">
-<input type="submit" name="Upload">
+<input type="submit" value="Upload">
 </form>
 <form enctype="multipart/form-data" action="subscribe.php" method="post">
-Would you like to subscribe to S3-upload message<input type="submit" name="Subscribe">
+Would you like to subscribe to S3-upload message<input type="submit" value="Subscribe">
 </form>
 </body>
 </html> 
+<?php
+if (isset($_GET['phone'])){
+$_SESSION['phone']=$_GET['phone']; 
+echo "$_SESSION['phone']";
+}
+?>
 
 
 

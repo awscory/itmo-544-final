9d7df3b1e766d20a233fbd6f1f8f6778f629b7c2
Sukanya Nagarajan <rajansuki@gmail.com>
2015-11-21 00:07:08 -0600

    test page

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 ?>
-
+<!DOCTYPE html>
 <html>
 <body>
 <form enctype="multipart/form-data" action="submit.php" method="post">
--- subscribe.php
+++ subscribe.php
@@ -3,7 +3,7 @@ session_start();
 echo "subscribed to number"
 echo $_POST["phone_hidden"];
 ?>
-
+<!DOCTYPE html>
 <html>
 <body>
 <a href="index.php"> Main page</a>

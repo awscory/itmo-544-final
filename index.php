1886648e203d62dbaed6285ecf2df03cdd3df167
Sukanya Nagarajan <rajansuki@gmail.com>
2015-11-20 22:12:05 -0600

    creating subscription page

--- index.php
+++ index.php
@@ -11,9 +11,11 @@ E-mail: <input type="text" name="email" value="****@***.iit.edu"><br>
 Phone : <input type="text" name="phone" value="312-000-0000"><br>
 <input type="hidden" name="MAX_FILE_SIZE" value="3000000"><br>
 Your File : <input type="file" name="userfile">
-<input type="submit">
+<input type="submit" name="Upload">
 </form>
-
+<form enctype="multipart/form-data" action="subscribe.php" method="post">
+Would you like to subscribe to S3-upload message<input type="submit" name="Subscribe">
+</form>
 </body>
 </html> 
 

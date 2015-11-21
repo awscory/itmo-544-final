b6360a25b8265479d1c8bf0f070812e144f75648
Sukanya Nagarajan <rajansuki@gmail.com>
2015-11-21 00:14:55 -0600

    test page

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ session_start();
 <form enctype="multipart/form-data" action="submit.php" method="post">
 User Name: <input type="text" name="username" value="sukanya"><br>
 E-mail: <input type="text" name="email" value="****@***.iit.edu"><br>
-Phone : <input type="text" name="phone" value="312-000-0000"><br>
+Phone : <input type="text" id="phone" name="phone" value="312-000-0000"><br>
 <input type="hidden" name="MAX_FILE_SIZE" value="3000000"><br>
 Your File : <input type="file" name="userfile">
 <input type="submit" value="Upload">

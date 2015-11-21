c39ebdf88f10db1924571f5dd8dcd365440f0055
Sukanya Nagarajan <rajansuki@gmail.com>
2015-11-21 12:06:35 -0600

    refer js in index file

--- index.php
+++ index.php
@@ -14,19 +14,13 @@ Your File : <input type="file" name="userfile">
 <input type="submit" value="Upload">
 </form>
 <form enctype="multipart/form-data" action="subscribe.php" method="post">
-<input type="hidden" id="phone_hidden" name="phone_hidden"/>
+<input type="text" id="phoneNo" name="phoneNo"/>
 Would you like to subscribe to S3-upload message<input type="submit" value="Subscribe">
 </form>
-<?php
-if (isset($_GET["phone"])){
-$_SESSION["phone"]=$_GET["phone"]; 
-print_r($_SESSION);
-}
-?>
+
 </body>
 </html> 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<script src="https://cdn.rawgit.com/sukanyaN/itmo-544-final/master/js/copytext.js"></script>
+
 
 
 
